e19020282dc8b718f3a1b0551086ec65a901179a
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-02-19 11:48:12 +0000

    Create new check.py file and move all functions that perform validation checks to file

---
+++ checks.py
@@ -0,0 +1,87 @@
+# Functions that carry out validation checks
+
+def user_quits(user_input):
+    """
+    Checks if the user has input 'q' to quit
+    Returns the user to the first menu
+    """
+    if user_input == "q":
+        print_colour("Quitting....please wait...", "magenta")
+        return True
+    return False
+
+
+def check_username(username):
+    """
+    Data validation of the username input by the user
+    Checks the string is a minimum of 6 characters
+    Checks the username does not match any other usernames already logged
+    Returns False if validation is not valid
+    """
+    usernames = logins.col_values(1)
+
+    username_length = len(username)
+
+    if username_length < 6:
+        print_colour("That's too short! Enter at least 6 characters", "grey")
+        return False
+    elif username in usernames:
+        print_colour("Not available. Please try something else", "grey")
+        return False
+    else:
+        return True
+
+
+def check_password(password):
+    """
+    Checks password
+    """
+    password_length = len(password)
+    # code for checking if password contains integer from:
+    # https://www.geeksforgeeks.org/password-validation-in-python/
+    if not any(char.isdigit() for char in password):
+        print_colour("You forgot to include a number! Try again", "grey")
+        return False
+    if password_length < 6:
+        print_colour("That's too short! Try again", "grey")
+        return False
+    return True
+
+
+def check_login_details(username, password):
+    """
+    Checks the users inputted username and password
+    matches the saved information in the worksheet
+    """
+    users_dict = logins.get_all_records()
+    if username in usernames_list:
+        current_user = next(
+            (x for x in users_dict if x["username"] == username)
+            )
+    if username not in usernames_list:
+        print_colour("Username not found. Try again\n", "grey")
+        return False
+    stored_password = current_user.get("password")
+    if password == stored_password:
+        print_colour("User account found. Please wait...\n", "cyan")
+    else:
+        print_colour("Password incorrect. Try again\n", "grey")
+        return False
+    return True
+
+
+def check_login_choice(choice):
+    """
+    Validates the users input from the login menu
+    Triggers the relevant function
+    """
+    try:
+        if int(choice) > 4 or int(choice) < 1:
+            raise ValueError(
+                "Please enter a number between 1 and 4"
+            )
+    except ValueError:
+        print("Invalid data. Please enter a number between 1 and 4")
+        return False
+   
+    return True
--- run.py
+++ run.py
@@ -85,76 +85,6 @@ def create_account():
     user_login()
 
 
-def user_quits(user_input):
-    """
-    Checks if the user has input 'q' to quit
-    Returns the user to the first menu
-    """
-    if user_input == "q":
-        print_colour("Quitting....please wait...", "magenta")
-        return True
-    return False
-
-
-def check_username(username):
-    """
-    Data validation of the username input by the user
-    Checks the string is a minimum of 6 characters
-    Checks the username does not match any other usernames already logged
-    Returns False if validation is not valid
-    """
-    usernames = logins.col_values(1)
-
-    username_length = len(username)
-
-    if username_length < 6:
-        print_colour("That's too short! Enter at least 6 characters", "grey")
-        return False
-    elif username in usernames:
-        print_colour("Not available. Please try something else", "grey")
-        return False
-    else:
-        return True
-
-
-def check_password(password):
-    """
-    Checks password
-    """
-    password_length = len(password)
-    # code for checking if password contains integer from:
-    # https://www.geeksforgeeks.org/password-validation-in-python/
-    if not any(char.isdigit() for char in password):
-        print_colour("You forgot to include a number! Try again", "grey")
-        return False
-    if password_length < 6:
-        print_colour("That's too short! Try again", "grey")
-        return False
-    return True
-
-
-def check_login_details(username, password):
-    """
-    Checks the users inputted username and password
-    matches the saved information in the worksheet
-    """
-    users_dict = logins.get_all_records()
-    if username in usernames_list:
-        current_user = next(
-            (x for x in users_dict if x["username"] == username)
-            )
-    if username not in usernames_list:
-        print_colour("Username not found. Try again\n", "grey")
-        return False
-    stored_password = current_user.get("password")
-    if password == stored_password:
-        print_colour("User account found. Please wait...\n", "cyan")
-    else:
-        print_colour("Password incorrect. Try again\n", "grey")
-        return False
-    return True
-
-
 def update_worksheet_logins(username, password):
     """
     Saves the username and password to google sheets
@@ -222,23 +152,6 @@ def display_login_options():
             display_about()
         else:
             display_login_options()
-
-
-def check_login_choice(choice):
-    """
-    Validates the users input from the login menu
-    Triggers the relevant function
-    """
-    try:
-        if int(choice) > 4 or int(choice) < 1:
-            raise ValueError(
-                "Please enter a number between 1 and 4"
-            )
-    except ValueError:
-        print("Invalid data. Please enter a number between 1 and 4")
-        return False
-   
-    return True
 
 
 def user_login():
